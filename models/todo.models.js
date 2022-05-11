abfe89c3adf7399888b62ec6a552bd198e466182
raphtis <angelvelazquez20@gmail.com>
2022-05-10 23:02:31 -0500

    Updated color scheme and fonts

--- models/todo.models.js
+++ models/todo.models.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const TodoSchema = new mongoose.Schema({
   text: {
     type: String, 
-    required: true, 
-    minlength: [3, 'Todo item must be at least 3 characters.']
+    required: true,
   }
 }, { timestamps: true });
 
